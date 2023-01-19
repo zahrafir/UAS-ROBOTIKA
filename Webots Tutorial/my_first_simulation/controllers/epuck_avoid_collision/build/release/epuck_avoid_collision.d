1f00ea17fe0226740234c0bb00ac8e01376bb46e
zahrafir <94789992+zahrafir@users.noreply.github.com>
2023-01-19 12:29:46 +0700

    Add files via upload

---
+++ Webots Tutorial/my_first_simulation/controllers/epuck_avoid_collision/build/release/epuck_avoid_collision.d
@@ -0,0 +1,6 @@
+build/release/epuck_avoid_collision.o: epuck_avoid_collision.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h
---
+++ Webots Tutorial/my_first_simulation/controllers/four_wheeled_collision_avoidance/build/release/four_wheeled_collision_avoidance.d
@@ -0,0 +1,7 @@
+build/release/four_wheeled_collision_avoidance.o: \
+ four_wheeled_collision_avoidance.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h
